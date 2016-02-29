d571f247d62b340f1c8bb0058f0e72ce3e35b83c
Michael Hart <michael.hart1994@gmail.com>
2016-02-29 18:50:40 +0000

    Initial write of vdp.vhd with entity instantiation and signals

--- hardware_files/vdp.vhd
+++ hardware_files/vdp.vhd
@@ -29,12 +29,40 @@ ENTITY vdp IS
 END vdp;
 
 ARCHITECTURE rtl OF vdp IS
+	SIGNAL db_finish, rcb_finish : std_logic;
+	SIGNAL dbb_delaycmd : std_logic; -- RCB to DB
+	SIGNAL dbb_bus : db_2_rcb; -- DB to RCB
 BEGIN
 
---
--- for you to write
---
+	-- Instantiate draw block entity
+	DB1 : ENTITY db GENERIC MAP (vsize => VSIZE) PORT MAP ( 
+		clk => clk, 
+		reset => reset,
+		hdb => hdb,
+		dav => dav,
+		hdb_busy => hdb_busy,
+		dbb_bus => dbb_bus, 
+		dbb_delaycmd => dbb_delaycmd,
+		db_finish => db_finish
+	);
 
-END rtl;      
+		
+	-- Instantiate RAM control block entity
+	RCB1 : ENTITY rcb GENERIC MAP (vsize => VSIZE) PORT MAP (
+		clk => clk,
+		reset => reset,
+		dbb_bus => dbb_bus,
+		dbb_delaycmd => dbb_delaycmd,
+		vdout => vdout,
+		vdin => vdin,
+		vwrite => vwrite,
+		vaddr => vaddr,
+		rcb_finish => rcb_finish
+	);
+
+	-- finish signal is output of AND gate, with inputs db_finish and rcb_finish
+	finish <= db_finish AND rcb_finish;
+
+END rtl;
 
 
