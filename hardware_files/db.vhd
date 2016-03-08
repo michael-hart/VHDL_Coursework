c7dd4c2d4c76e834a66b45868df2b246a50a9089
Meng Kiang Seah <Meng Kiang Seah>
2016-03-08 18:11:41 +0000

    Updated registers. Signals to and from draw_any_octant created. In and out multiplexer processes made.

--- hardware_files/db.vhd
+++ hardware_files/db.vhd
@@ -24,84 +24,116 @@ ENTITY db IS
 END db;
 
 ARCHITECTURE rtl OF db IS
--- Possible size changes if vsize!=6
+-- All registers from diagram.
 SIGNAL hdb_reg : STD_LOGIC_VECTOR(15 DOWNTO 0);
--- Consider spliting x and y, pen and op.
 SIGNAL xy_new_reg : STD_LOGIC_VECTOR(11 DOWNTO 0);
 SIGNAL xy_old_reg : STD_LOGIC_VECTOR(11 DOWNTO 0);
--- SIGNAL pen_op_reg : STD_LOGIC_VECTOR(3 DOWNTO 0);
+SIGNAL pen_op_reg : STD_LOGIC_VECTOR(3 DOWNTO 0);
 
+-- Multiplexer signals, controlled by DB_FSM
+SIGNAL mux_in, mux_out : std_logic;
+
+-- Connections of draw_any_octant
+-- From OCTANT_CMB
 SIGNAL swapxy, negx, negy, xbias : STD_LOGIC;
+-- I/O
+SIGNAL x_in, y_in, x_out, y_out : std_logic_vector(5 DOWNTO 0);
+-- To/from DB_FSM
+SIGNAL draw, resetx, done, disable : std_logic;
+
+-- For DB_FSM
 SIGNAL db_fsm_state, db_fsm_nstate : state_db;
+
 BEGIN
 
--- Process for clocked registers.
-REG : PROCESS BEGIN
-	WAIT UNTIL clk'EVENT AND clk = '1';
-	hdb_reg <= hdb;
-	xy_new_reg <= hdb_reg(13 DOWNTO 2);
-	xy_old_reg <= xy_new_reg;
-	pen_op_reg <= (hdb(15 DOWNTO 14), hdb(1 DOWNTO 0));
-END PROCESS REG;
+	-- Process for clocked registers.
+	REG : PROCESS BEGIN
+		WAIT UNTIL clk'EVENT AND clk = '1';
+		hdb_reg <= hdb;
+		xy_new_reg <= hdb_reg(13 DOWNTO 2);
+		xy_old_reg <= xy_new_reg;
+		pen_op_reg <= (hdb(15 DOWNTO 14), hdb(1 DOWNTO 0));
+	END PROCESS REG;
 
--- Octant-cmb block
-OCTANT_CMB :PROCESS (xy_new_reg, xy_old_reg) BEGIN
-	-- Create variables.
-	VARIABLE dx, dy : std_logic_vector(6 DOWNTO 0);
-	VARIABLE x_more, y_more, x_y_comp : std_logic;
-	VARIABLE oct_together : std_logic_vector(2 DOWNTO 1);
+	-- Octant-cmb block
+	OCTANT_CMB :PROCESS (xy_new_reg, xy_old_reg) BEGIN
+		-- Create variables.
+		VARIABLE dx, dy : std_logic_vector(6 DOWNTO 0);
+		VARIABLE x_more, y_more, x_y_comp : std_logic;
+		VARIABLE oct_together : std_logic_vector(2 DOWNTO 1);
 
-	-- Assign values.
-	dx := signed(unsigned(xy_new_reg(11 DOWNTO 6))-unsigned(xy_old_reg(11 DOWNTO 6)));
-	dy := signed(unsigned(xy_new_reg(5 DOWNTO 0))-unsigned(xy_old_reg(5 DOWNTO 0)));
+		-- Assign values.
+		dx := signed(unsigned(xy_new_reg(11 DOWNTO 6))-unsigned(xy_old_reg(11 DOWNTO 6)));
+		dy := signed(unsigned(xy_new_reg(5 DOWNTO 0))-unsigned(xy_old_reg(5 DOWNTO 0)));
 
-	IF dx >= 0 THEN
-		x_more := '1';
+		IF dx >= 0 THEN
+			x_more := '1';
+		ELSE
+			x_more := '0';
+		END IF;
+
+		IF dy >= 0 THEN
+			y_more := '1';
+		ELSE
+			y_more := '0';
+		END IF;
+
+		IF abs(dy) >= abs(dx) THEN
+			x_y_comp := '1';
+		ELSE
+			x_y_comp := '0';
+		END IF;
+
+		-- Concatenate for case statement.
+		oct_together := (x_more, y_more, x_y_comp);
+
+		CASE oct_together IS
+			WHEN "110" | "111"	=> 	negx <= '0';
+															negy <= '0';
+			WHEN "000" | "001"	=> 	negx <= '1';
+															negy <= '1':
+			WHEN "011" : "100"	=> 	negx <= '0';
+															negy <= '1';
+			WHEN "010" | "101"	=> 	negx <= '1';
+															negy <= '0';
+			WHEN OTHERS 				=> 	NULL;
+		END CASE;
+
+		-- No need to include in case. Makes easier to read.
+		swapxy <= x_y_comp;
+		xbias <= '1';
+	END PROCESS OCTANT_CMB;
+
+	-- Multiplexer at draw_any_octant input.
+	IN_MUX : PROCESS (mux_in, xy_new_reg, xy_old_reg) BEGIN
+
+	IF mux_in = '1' THEN
+		x_in <= xy_new_reg(11 DOWNTO 6);
+		y_in <= xy_new_reg(5 DOWNTO 0);
 	ELSE
-		x_more := '0';
+		x_in <= xy_old_reg(11 DOWNTO 6);
+		y_in <= xy_old_reg(5 DOWNTO 0);
 	END IF;
 
-	IF dy >= 0 THEN
-		y_more := '1';
+	END PROCESS IN_MUX;
+
+	-- Multiplexer at draw_any_octant output.
+	OUT_MUX : PROCESS (mux_out, x_in, y_in, x_out, y_out) BEGIN
+
+	IF mux_out = '1' THEN
+		dbb_bus.X <= x_in;
+		dbb_bus.Y <= y_in;
 	ELSE
-		y_more := '0';
+		dbb_bus.X <= x_out;
+		dbb_bus.Y <= y_out
 	END IF;
 
-	IF abs(dy) >= abs(dx) THEN
-		x_y_comp := '1';
-	ELSE
-		x_y_comp := '0';
-	END IF;
+	END PROCESS OUT_MUX;
 
-	-- Concatenate for case statement.
-	oct_together := (x_more, y_more, x_y_comp);
+	FSM : PROCESS BEGIN
 
-	CASE oct_together IS
-		WHEN "110" | "111"	=> 	negx <= '0';
-														negy <= '0';
-		WHEN "000" | "001"	=> 	negx <= '1';
-														negy <= '1':
-		WHEN "011" : "100"	=> 	negx <= '0';
-														negy <= '1';
-		WHEN "010" | "101"	=> 	negx <= '1';
-														negy <= '0';
-		WHEN OTHERS 				=> 	NULL;
-	END CASE;
+	END PROCESS FSM;
 
-	-- No need to include in case. Makes easier to read.
-	swapxy <= x_y_comp;
-	xbias <= '1';
-END PROCESS OCTANT_CMB;
-
-FSM : PROCESS BEGIN
-	WAIT UNTIL clk'EVENT AND clk = '1';
-	IF reset = '1' THEN
-		db_fsm_state <= swait;
-	ELSE
-		db_fsm_state <= db_fsm_nstate;
-	END IF;
-END PROCESS FSM;
-
-N_FSM : PROCESS () BEGIN
+	N_FSM : PROCESS () BEGIN
 
 END rtl;
